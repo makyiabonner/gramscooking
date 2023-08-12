0786a52165237377a173a9d44e49df00497f39e6
makyia bonner <makyiabonner@gmail.com>
2023-08-12 18:38:25 -0400

    installed font awesome package

--- src/components/Home_foodcard/home_foodcard.js
+++ src/components/Home_foodcard/home_foodcard.js
@@ -6,23 +6,24 @@ export default function HomeFoodCard(props) {
 
     return (
         <div className={styles.card}>
-            <div className={styles.img_container}>
-                <img src={props.img} alt="" className={styles.food_img} />
-            </div>
-            <div>
-                <div className={styles.span_textbox}>
-                    <svg className={styles.profile} xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="25">
-                        <path d="M480-481q-66 0-108-42t-42-108q0-66 42-108t108-42q66 0 108 42t42 108q0 66-42 108t-108 42ZM160-160v-94q0-38 19-65t49-41q67-30 128.5-45T480-420q62 0 123 15.5t127.921 44.694q31.301 14.126 50.19 40.966Q800-292 800-254v94H160Zm60-60h520v-34q0-16-9.5-30.5T707-306q-64-31-117-42.5T480-360q-57 0-111 11.5T252-306q-14 7-23 21.5t-9 30.5v34Zm260-321q39 0 64.5-25.5T570-631q0-39-25.5-64.5T480-721q-39 0-64.5 25.5T390-631q0 39 25.5 64.5T480-541Zm0-90Zm0 411Z"/>
-                    </svg>
-                    <span className={styles.span}>Muffin Group</span>
-                    <span className={styles.span}> at</span>
-                    <svg className={styles.time} xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="25">
-                        <path d="m627-287 45-45-159-160v-201h-60v225l174 181ZM480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-82 31.5-155t86-127.5Q252-817 325-848.5T480-880q82 0 155 31.5t127.5 86Q817-708 848.5-635T880-480q0 82-31.5 155t-86 127.5Q708-143 635-111.5T480-80Zm0-400Zm0 340q140 0 240-100t100-240q0-140-100-240T480-820q-140 0-240 100T140-480q0 140 100 240t240 100Z"/>
-                    </svg>
-                    <span className={styles.span}>date</span>
+                <div className={styles.img_container}>
+                    <img src={props.img} alt="" className={styles.food_img} />
                 </div>
-                <h4 className={styles.h4}>{props.title}</h4>
-                <p className={styles.p}>{props.context}</p>
+                <div className={styles.textarea}>
+                    <div className={styles.span_textbox}>
+                        <svg className={styles.profile} xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="25">
+                            <path d="M480-481q-66 0-108-42t-42-108q0-66 42-108t108-42q66 0 108 42t42 108q0 66-42 108t-108 42ZM160-160v-94q0-38 19-65t49-41q67-30 128.5-45T480-420q62 0 123 15.5t127.921 44.694q31.301 14.126 50.19 40.966Q800-292 800-254v94H160Zm60-60h520v-34q0-16-9.5-30.5T707-306q-64-31-117-42.5T480-360q-57 0-111 11.5T252-306q-14 7-23 21.5t-9 30.5v34Zm260-321q39 0 64.5-25.5T570-631q0-39-25.5-64.5T480-721q-39 0-64.5 25.5T390-631q0 39 25.5 64.5T480-541Zm0-90Zm0 411Z"/>
+                        </svg>
+                        <span className={styles.span}>Muffin Group</span>
+                        <span className={styles.span}> at</span>
+                        <svg className={styles.time} xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="25">
+                            <path d="m627-287 45-45-159-160v-201h-60v225l174 181ZM480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-82 31.5-155t86-127.5Q252-817 325-848.5T480-880q82 0 155 31.5t127.5 86Q817-708 848.5-635T880-480q0 82-31.5 155t-86 127.5Q708-143 635-111.5T480-80Zm0-400Zm0 340q140 0 240-100t100-240q0-140-100-240T480-820q-140 0-240 100T140-480q0 140 100 240t240 100Z"/>
+                        </svg>
+                        <span className={styles.span}>date</span>
+                    </div>
+                    <h4 className={styles.h4}>{props.title}</h4>
+                    <p className={styles.p}>{props.context}</p>
+                </div>
                 <div className={styles.bottom_content}>
                     <button
                         className={`${isActive ? `${styles.active} ${styles.like_button}` : `${styles.inactive} ${styles.like_button}`}`}
@@ -42,7 +43,6 @@ export default function HomeFoodCard(props) {
                         <a className={styles.a}>Read more</a>
                     </div>
                 </div>
-            </div>
         </div>
     );
 }
--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -1,25 +1,53 @@
 import styles from './nav.module.css';
-import menu from '../../icons/menu.svg'
+import menu from '../../icons/menu.svg';
+import close from '../../icons/close-button.svg';
+import logo from '../../icons/logo.svg';
+import React, { useState } from 'react';
 
 export default function Nav(){
+    const [isActive, setIsActive ] = useState(false)
     return (
         <>
             <nav className={styles.nav}>
-                <img src="" alt="" className={styles.logo} />
-                <div className={styles.div}>
-                    <ul className={styles.nav_list}>
-                        <li className={styles.list_item}>Home</li>
-                        <li className={styles.list_item}>Recipes</li>
-                        <li className={styles.list_item}>Recipe</li>
-                        <li className={styles.list_item}>About us</li>
-                        <li className={styles.list_item}>Catering</li>
-                    </ul>
+                <section>
+                    <img src={logo} alt="logo" className={styles.logo} />
                     <div className={styles.div}>
-                        <img src={menu} alt="" className={styles.hamburger_menu} />
-                        <button className={styles.btn}>Buy now</button>
+                        <ul className={styles.nav_list}>
+                            <li className={styles.list_item}>Home</li>
+                            <li className={styles.list_item}>Recipes</li>
+                            <li className={styles.list_item}>Recipe</li>
+                            <li className={styles.list_item}>About us</li>
+                            <li className={styles.list_item}>Catering</li>
+                        </ul>
+                        <div className={styles.div}>
+                            <img src={menu} alt="" className={styles.hamburger_menu} />
+                            <button className={styles.btn}>Buy now</button>
+                        </div>
                     </div>
-                </div>
+                </section>
             </nav>
+            <section className={styles.mobile_menu}>
+                <div className={styles.mobile_close_div}>
+                    <button className={styles.close_button}>
+                        <img src={close} alt='close button' className={styles.close_button_icon}/>
+                    </button>
+                </div>
+                <div className={styles.mobile_buy_div}>
+                    <button className={styles.mobile_buy_btn}>Buy now</button>
+                </div>
+                <ul className={styles.mobile_list}>
+                    <li className={styles.mobile_list_item}>Home</li>
+                    <li className={styles.mobile_list_item}>Recipes</li>
+                    <li className={styles.mobile_list_item}>Recipe</li>
+                    <li className={styles.mobile_list_item}>About us</li>
+                    <li className={styles.mobile_list_item}>Catering</li>
+                </ul>
+                <ul className={styles.social_media_div}>
+                    <li><a className={styles.social_media_logo}></a></li>
+                    <li><a className={styles.social_media_logo}></a></li>
+                    <li><a className={styles.social_media_logo}></a></li>
+                </ul>
+            </section>
         </>
     )
 }
