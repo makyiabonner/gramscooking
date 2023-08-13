6985f649490d0cfff98dda9b34acac6b19a43e86
makyia bonner <makyiabonner@gmail.com>
2023-08-12 22:57:48 -0400

    fixed bugs for production build

--- src/components/Button/button.js
+++ src/components/Button/button.js
@@ -2,7 +2,7 @@ import styles from './button.module.css';
 import arrow from '../../icons/arrow.svg';
 
 export default function Button(props){
-    {if (props.type.toLowerCase() === 'dark'){
+    if (props.type.toLowerCase() === 'dark'){
         return(
             <button className={styles.red_button}>
                 <p className={styles.p}>{props.content}</p>
@@ -16,7 +16,6 @@ export default function Button(props){
             <p className={styles.p}>{props.content}</p>
             <img className={styles.light_arrow} src={arrow} alt="arrow" />
         </button>
-        )}
-}
-    
+        )
+    }
 }
